a8465804aa707b721a74e195ea5f4261cec1efc2
Paul <pmacellaro7@aol.com>
2023-04-28 17:08:46 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  const names = [`Guadalupe", "Ollie", "Aki`];
+  const messages = writeCards(names);
+  console.log(messages);
+
+
+
+let i= 10
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+      console.log(i);
+    }
+  }
